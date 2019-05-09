431175c451fdb7bb2614a9c0a905c8aa6e3fcf99
Rodrigo Papadopulo <rpapadopulo.2106@gmail.com>
2019-05-09 00:01:06 -0300

    initial commit

---
+++ build/precache-manifest.927160fbc2721a3a0509c5cb27face80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264aef4354dc768382d1dafc65859dcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5378458acfce6a8228a",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "477b7ea793ecd629e2e4",
+    "url": "/static/css/main.37e35700.chunk.css"
+  },
+  {
+    "revision": "f5378458acfce6a8228a",
+    "url": "/static/js/2.b2c62dee.chunk.js"
+  },
+  {
+    "revision": "477b7ea793ecd629e2e4",
+    "url": "/static/js/main.50fbdee9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
