dcc4e52768a32e5a252a0617f6c0a3172969215f
Rodrigo Papadopulo <rpapadopulo.2106@gmail.com>
2019-05-12 19:46:28 -0300

    Avances en tablas y pantallas

--- src/ListadoAlojamientos.js
+++ src/ListadoAlojamientos.js
@@ -1,21 +1,55 @@
 import React, { Component } from "react";
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
+import axios from "axios";
  
 class ListadoAlojamientos extends Component {
+  state = {
+    alojamientos: []
+  };
+
+  componentDidMount() {
+    axios
+    //.get("https://alojapp-backend.herokuapp.com/alojamiento/get")
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then(response => {
+      const newData = response.data.map(c => {
+        return {
+          id: c.id,
+          name: c.name,
+          username: c.username
+        };
+      });
+      const newState = Object.assign({}, this.state, {
+        alojamientos: newData
+      });
+      this.setState(newState);
+    })
+    .catch(error => console.log(error));
+  };
+
   render() {
-    return (
-      <div>
-        <h2>STUFF</h2>
-        <p>Mauris sem velit, vehicula eget sodales vitae,
-        rhoncus eget sapien:</p>
-        <ol>
-          <li>Nulla pulvinar diam</li>
-          <li>Facilisis bibendum</li>
-          <li>Vestibulum vulputate</li>
-          <li>Eget erat</li>
-          <li>Id porttitor</li>
-        </ol>
-      </div>
-    );
+    const columns = [{
+      Header: 'Id',
+      accessor: 'id' // String-based value accessors!
+    }, {
+      Header: 'Nombre',
+      accessor: 'name'
+    }, {
+      Header: 'Nombre de Usuario',
+      accessor: 'username' // Custom value accessors!
+    }]
+   
+    return <ReactTable
+      data={this.state.alojamientos}
+      columns={columns}
+      defaultPageSize={5}
+      previousText={'Anterior'}
+      nextText={'Siguiente'}
+      pageText={'Página'}
+      ofText={'de'}
+      rowsText={'filas'}
+    />
   }
 }
  
